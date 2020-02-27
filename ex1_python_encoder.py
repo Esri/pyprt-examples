b365555016056b4d9e3a9ec6488cf6116dab11d6
Camille Lechot <clechot@esri.com>
2020-02-27 10:39:56 +0100

    fixed path to data folder

--- ex1_python_encoder.py
+++ ex1_python_encoder.py
@@ -23,7 +23,7 @@ CS_FOLDER = os.path.dirname(os.path.realpath(__file__))
 
 
 def asset_file(filename):
-    return os.path.join(os.path.dirname(CS_FOLDER), 'examples', 'data', filename)
+    return os.path.join(CS_FOLDER, 'data', filename)
 
 
 # PRT Initialization
