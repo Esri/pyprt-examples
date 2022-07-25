d54417088c43b7068c88074af00b173b41a7c8e7
Simon Haegler <shaegler@esri.com>
2022-07-25 16:43:28 +0200

    Example 10: add header and small manual

--- ex10_update_scene_layer_package.py
+++ ex10_update_scene_layer_package.py
@@ -1,3 +1,25 @@
+# Copyright (c) 2012-2022 Esri R&D Center Zurich
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#   https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# A copy of the license is available in the repository's LICENSE file.
+
+# Example Steps:
+# 1. At first run, the script will create a new Scene Layer called 'PyPRT_Ex10_Scene_Layer'
+# 2. Create a Web Scene with this new layer
+# 3. Retrieve the item ID of the above layer and set it for 'TARGET_SCENE_LAYER_ID' below
+# 4. Now change the POPULATION_DENSITY_MODE to 'logarithmic' and re-run the script
+# 5. After a while, the Web Scene will update automatically.
+
 import sys
 import getpass
 import os.path
